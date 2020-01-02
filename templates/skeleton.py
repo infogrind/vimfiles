d866a13b7380db3e2b3938fb66d1a1cda220337a
Marius Kleiner <kleiner@gmail.com>
2020-01-02 17:51:10 +0100

    Make spacing PEP conform

--- templates/skeleton.py
+++ templates/skeleton.py
@@ -9,64 +9,63 @@ output = "-"
 
 
 def main():
-  args = parse_options(sys.argv[1:])
+    args = parse_options(sys.argv[1:])
 
-  print("Output: ", output)
-  debug("If you see this, verbose mode is enabled.")
+    print("Output: ", output)
+    debug("If you see this, verbose mode is enabled.")
 
-  # Complete the program here. Enjoy!
+    # Complete the program here. Enjoy!
 
 
 def usage():
-  print("""Usage: <syntax goes here>
+    print("""Usage: <syntax goes here>
 
-  <description goes here>
+    <description goes here>
 
-  Options:
-  -h        Show this help text.
-  -v        Display verbose output.
-  -o <file> Write output to <file> or to stdout if <file> is '-'.
-  """)
+    Options:
+    -h              Show this help text.
+    -v              Display verbose output.
+    -o <file> Write output to <file> or to stdout if <file> is '-'.
+    """)
 
 
 def parse_options(args):
-  
-  # Access global variables
-  global verbose, output
 
-  # Parse options using Getopt; display an error and exit if options could not
-  # be parsed.
-  try:
-    opts, args = getopt.getopt(args, "o:hv")
-  except getopt.GetoptError as err:
-    print(str(err))
-    usage()
-    sys.exit(2)
+    # Access global variables
+    global verbose, output
 
-  # Set variables according to options
-  for opt, val in opts:
-    if opt == "-h":
-      usage()
-      sys.exit()
+    # Parse options using Getopt; display an error and exit if options could not
+    # be parsed.
+    try:
+        opts, args = getopt.getopt(args, "o:hv")
+    except getopt.GetoptError as err:
+        print(str(err))
+        usage()
+        sys.exit(2)
 
-    elif opt == "-v":
-      verbose = True
+    # Set variables according to options
+    for opt, val in opts:
+        if opt == "-h":
+            usage()
+            sys.exit()
 
-    elif opt == "-o":
-      output = val
-    else:
-      assert False, "unhandled option"
+        elif opt == "-v":
+            verbose = True
 
-  return args
+        elif opt == "-o":
+            output = val
+        else:
+            assert False, "unhandled option"
+
+    return args
 
 
 def debug(s):
-  if verbose:
-    sys.stderr.write(s + "\n")
+    if verbose:
+        sys.stderr.write(s + "\n")
 
 
 # This is the most important line: it calls the main function if this program is
 # called directly.
 if __name__ == "__main__":
-  main()
-
+    main()
